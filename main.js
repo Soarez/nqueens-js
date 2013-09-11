c8fb5981e6ee851b5a4808f0fbaaae8fe27a813d
Igor Soarez <igorsoarez@gmail.com>
2013-09-11 14:45:59 +0100

    make main requireable

--- main.js
+++ main.js
@@ -2,33 +2,43 @@
 
 var util = require('util');
 
+module.exports = main;
+
+if (require.main !== module)
+  return;
+
 if (process.argv.length < 4) {
   console.log('Usage: %s <boardSize> <maxGens>', process.argv[1]);
   return;
 }
 
-size = parseInt(process.argv[2]);
-maxgen = parseInt(process.argv[3]);
+main(parseInt(process.argv[2]), parseInt(process.argv[3]), true);
 
-console.log('Running with size %s in %s generations.', size, maxgen);
+function main(size, maxgen, log) {
+  if (log)
+    console.log('Running with size %s in %s generations.', size, maxgen);
 
-var NQueens = require('./nqueens');
+  var NQueens = require('./nqueens');
 
-var solver = NQueens({ boardSize: size, maxGenerations: maxgen });
+  var solver = NQueens({ boardSize: size, maxGenerations: maxgen });
 
-solver.run();
+  solver.run();
 
-var best = solver.best();
-console.log('Best solution found: %s', JSON.stringify(best));
+  var best = solver.best();
+  if (log)
+    console.log('Best solution found: %s', JSON.stringify(best));
 
-// console.log('All solutions:');
-// for (var specimen in solver.getPopulation())
-//   console.log(specimen);
+  // console.log('All solutions:');
+  // for (var specimen in solver.getPopulation())
+  //   console.log(specimen);
 
-var gene = best.gene;
+  var gene = best.gene;
 
-for(var i = 0; i < size; ++i)
-  for(var j = 0; j < size; ++j)
-    util.print('|'
-      + (gene[i] === j ? 'Q' : ' ')
-      + (j === size - 1 ? '|\n' : ''));
+  if (log)
+    for(var i = 0; i < size; ++i)
+      for(var j = 0; j < size; ++j)
+        util.print('|'
+          + (gene[i] === j ? 'Q' : ' ')
+          + (j === size - 1 ? '|\n' : ''));
+}
+
